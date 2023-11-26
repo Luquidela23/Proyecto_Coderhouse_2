996640f99d3cfd2cff43976d0c551344fdd7a215
Lucas de la Arena Coll <lucasdlarena@gmail.com>
2023-11-26 20:54:51 -0300

    Home_blog revisado

--- blog/views.py
+++ blog/views.py
@@ -1,54 +1,53 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 from .models import Articulo
 from .forms import ArticuloForm
 
 def home_blog(request):
-    return render(request, 'blog/home.html')
+    # Muestra la lista de artículos en la página de inicio
+    articulos = Articulo.objects.all()
+    return render(request, 'blog/home_blog.html', {'articulos': articulos})
 
 def about(request):
+    # Página de "Acerca de"
     return render(request, 'blog/about.html')
 
-def lista_articulos(request):
-    articulos = Articulo.objects.all()
-    return render(request, 'blog/lista_articulos.html', {'articulos': articulos})
-
 def detalle_articulo(request, articulo_id):
+    # Detalle de un artículo específico
     articulo = get_object_or_404(Articulo, pk=articulo_id)
     return render(request, 'blog/detalle_articulo.html', {'articulo': articulo})
 
-@login_required
 def crear_articulo(request):
+    # Crear un nuevo artículo
     if request.method == 'POST':
         form = ArticuloForm(request.POST)
         if form.is_valid():
-            nuevo_articulo = form.save(commit=False)
-            nuevo_articulo.autor = request.user
-            nuevo_articulo.save()
-            return redirect('blog:lista_articulos')
+            form.save()
+            return render(request, 'blog/articulo_creado.html')
     else:
         form = ArticuloForm()
-
     return render(request, 'blog/crear_articulo.html', {'form': form})
 
-@login_required
 def editar_articulo(request, articulo_id):
+    # Editar un artículo existente
     articulo = get_object_or_404(Articulo, pk=articulo_id)
-    if request.user == articulo.autor:
-        if request.method == 'POST':
-            form = ArticuloForm(request.POST, instance=articulo)
-            if form.is_valid():
-                form.save()
-                return redirect('blog:detalle_articulo', articulo_id=articulo.pk)
-        else:
-            form = ArticuloForm(instance=articulo)
-        return render(request, 'blog/editar_articulo.html', {'form': form})
+    if request.method == 'POST':
+        form = ArticuloForm(request.POST, instance=articulo)
+        if form.is_valid():
+            form.save()
+            return render(request, 'blog/articulo_editado.html')
     else:
-        return redirect('blog:lista_articulos')
+        form = ArticuloForm(instance=articulo)
+    return render(request, 'blog/editar_articulo.html', {'form': form, 'articulo': articulo})
 
-@login_required
 def eliminar_articulo(request, articulo_id):
+    # Eliminar un artículo
     articulo = get_object_or_404(Articulo, pk=articulo_id)
-    if request.user == articulo.autor:
+    if request.method == 'POST':
         articulo.delete()
-    return redirect('blog:lista_articulos')
+        return render(request, 'blog/articulo_eliminado.html')
+    return render(request, 'blog/eliminar_articulo.html', {'articulo': articulo})
+
+def lista_articulos(request):
+    # Lista de todos los artículos
+    articulos = Articulo.objects.all()
+    return render(request, 'blog/lista_articulos.html', {'articulos': articulos})
